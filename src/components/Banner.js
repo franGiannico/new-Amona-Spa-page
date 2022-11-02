16c4b8e1130702eeafb4f39cc868f96d8ba920c5
Francisco Javier Giannico Villalobos <fjgiannico@gmail.com>
2022-11-01 23:35:12 -0300

    resolviendo error en comp Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
                     <h1 className="title">Bienvenidos a Amona Spa</h1>
                     <p className="subtitle">Tu lugar de relajación, salud y armonía interior</p>
                     <div>
-                        <a className="" href="https://api.whatsapp.com/send?phone=5493515193175&text=hola,%20quiero%20reservar%20un%20turno..." target="_blank">Reservá tu turno!</a>
+                        <a className="" href="#" >Reservá tu turno</a>
                     </div>
                 </div>
                 <div className="">
